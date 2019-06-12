e1b6b97938d924a22ca7eed8d467e51532b61e82
ivaylotsachev <itsa4ev@gmail.com>
2019-06-12 22:00:23 +0300

    Updates

---
+++ precache-manifest.379702e7dd7c125012921c0caafc078f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1eea85430b8ce6d16e9133a14a1a833",
+    "url": "/neon-puzzle-game/index.html"
+  },
+  {
+    "revision": "78f59c0d93d15f9e9096",
+    "url": "/neon-puzzle-game/static/css/main.d8817dac.chunk.css"
+  },
+  {
+    "revision": "dbd9b9d1d09f56a8fe13",
+    "url": "/neon-puzzle-game/static/js/2.63a3d80a.chunk.js"
+  },
+  {
+    "revision": "78f59c0d93d15f9e9096",
+    "url": "/neon-puzzle-game/static/js/main.61cb9df8.chunk.js"
+  },
+  {
+    "revision": "4feb37f2f02bd8bbb307",
+    "url": "/neon-puzzle-game/static/js/runtime~main.bced1016.js"
+  }
+]);
